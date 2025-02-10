40a397ed39ec108ef1c47624403a864b551c0688
kaptast <kaptas.tamas@gmail.com>
2025-02-10 21:00:48 +0100

    create the basic structure

---
+++ src/lib/components/Header/MenuOpener.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let opened = $state(false);
+</script>
+
+<button
+	class="{opened
+		? 'before:top-1/2 before:origin-center before:rotate-45 after:top-1/2 after:origin-center after:-rotate-45'
+		: 'before:top-0 after:top-full'} relative block h-4 w-7 before:absolute before:top-0 before:right-0 before:left-0 before:h-0 before:w-full before:border-b-4 before:border-solid before:border-b-black before:transition-all before:duration-300 before:content-[''] after:absolute after:right-0 after:left-0 after:h-0 after:w-full after:border-b-4 after:border-solid after:border-b-black after:transition-all after:duration-300 after:content-['']"
+	aria-label="TODO: open the menu"
+	onclick={() => (opened = !opened)}
+>
+	<i
+		class="absolute top-1/2 block h-1 w-full overflow-hidden bg-black indent-[full] whitespace-nowrap transition-opacity duration-100 {opened
+			? 'opacity-0'
+			: ''}">Menu</i
+	>
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import '../app.css';
+
+	import * as m from '$lib/paraglide/messages.js';
+
+	import About from '$lib/components/About/About.svelte';
+	import Contact from '$lib/components/Contact/Contact.svelte';
+	import Images from '$lib/components/Images/Images.svelte';
+	import Footer from '$lib/components/Footer/Footer.svelte';
+	import Header from '$lib/components/Header/Header.svelte';
+	import Hero from '$lib/components/Hero/Hero.svelte';
+	import Map from '$lib/components/Map/Map.svelte';
+	import Wines from '$lib/components/Wines/Wines.svelte';
+</script>
+
+<main>
+	<ParaglideJS {i18n}>
+		<Header />
+		<Hero />
+		<About />
+		<Wines />
+		<Images />
+		<Map />
+		<Contact />
+		<Footer />
+	</ParaglideJS>
+</main>
