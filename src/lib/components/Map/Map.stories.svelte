e078517fbbde87b95b7f2e37a23534f62ee62607
kaptast <kaptas.tamas@gmail.com>
2025-02-18 21:38:13 +0100

    add a map

---
+++ src/lib/components/Map/Map.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Map from './Map.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Map',
+		component: Map
+	});
+</script>
+
+<Story name="Default" />
---
+++ src/lib/components/Map/Map.svelte
@@ -0,0 +1,11 @@
+<section id="map">
+	<iframe
+		title="Schenek Pince és Présház"
+		src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3352.0730809316437!2d18.7394618!3d47.440718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476a0dec5fd7d33b%3A0x8b59cdcef269da83!2zU2NoZW5layBQaW5jZSDDqXMgUHLDqXNow6F6!5e1!3m2!1shu!2shu!4v1739910429599!5m2!1shu!2shu"
+		width="600"
+		height="450"
+		style="border:0;"
+		loading="lazy"
+		referrerpolicy="no-referrer-when-downgrade"
+	></iframe>
+</section>
