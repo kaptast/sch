961fb0d011f80c95a6ae72924dd3e1a7eaa9ffb5
kaptast <kaptas.tamas@gmail.com>
2025-02-18 21:11:13 +0100

    create a hero

---
+++ src/lib/components/Hero/Hero.svelte
@@ -0,0 +1,11 @@
+<section id="hero" class="flex h-dvh w-full items-center justify-center">
+	<h2 class="text-7xl font-bold text-white uppercase">SCHENEK</h2>
+</section>
+
+<style>
+	section {
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-image: url(https://picsum.photos/800/700);
+	}
+</style>
