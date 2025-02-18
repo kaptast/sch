2db46861f8ce7cf39d32ae897fa6888e6366164f
kaptast <kaptas.tamas@gmail.com>
2025-02-18 21:22:45 +0100

    create the about section

---
+++ src/lib/components/About/About.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import About from './About.svelte';
+
+	const { Story } = defineMeta({
+		title: 'About',
+		component: About
+	});
+</script>
+
+<Story name="Default" />
---
+++ src/lib/components/About/About.svelte
@@ -0,0 +1,38 @@
+<section class="bg-amber-100/25 p-10 text-center text-yellow-800">
+	<h2 class="text-5xl font-semibold uppercase">SCHENEK</h2>
+
+	<h3 class="text-xl font-bold text-stone-800 uppercase">Bemutatkozás</h3>
+
+	<h4 class="my-6 text-lg font-semibold">Lorem ipsum</h4>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a odio at dolor egestas
+		blandit. Donec iaculis libero at libero ultricies iaculis. Nullam ultricies porttitor erat, sit
+		amet bibendum felis blandit sed. Donec vestibulum sed arcu sit amet aliquet. Integer in elit
+		pulvinar, porta risus ac, aliquet elit. Maecenas nulla risus, fermentum et condimentum sed,
+		consequat a diam. Aenean fermentum ante sed libero finibus, vitae sodales neque euismod. Mauris
+		porttitor augue sollicitudin ultrices scelerisque. Proin egestas aliquet lectus, ac convallis
+		ante posuere cursus. Cras ut mi magna. In hac habitasse platea dictumst. Morbi erat justo,
+		facilisis vitae orci quis, suscipit rutrum purus.
+	</p>
+
+	<div class="mx-auto my-6 h-px w-48 bg-yellow-800"></div>
+
+	<h4 class="my-6 text-lg font-semibold">Lorem ipsum</h4>
+	<p>
+		Aenean cursus elit elementum mi dictum, et condimentum mi convallis. Phasellus laoreet sem
+		dictum ex gravida vulputate. Nulla porttitor luctus auctor. Pellentesque vel viverra eros, a
+		pharetra nisl. Aliquam ultricies augue non eros volutpat ultricies eget sed elit. Nullam quis
+		metus diam. Phasellus in interdum ante. Sed at rhoncus lacus, sed mollis turpis.
+	</p>
+
+	<div class="mx-auto my-6 h-px w-48 bg-yellow-800"></div>
+
+	<h4 class="my-6 text-lg font-semibold">Lorem ipsum</h4>
+	<p>
+		Suspendisse potenti. In a luctus risus. Proin eu quam eget sapien tempus gravida. Nulla egestas
+		tempor urna, ut rutrum ex fringilla efficitur. Sed eu risus in ligula imperdiet laoreet. Nulla
+		sit amet varius arcu, id ornare massa. Maecenas mattis justo turpis, vel tristique magna
+		accumsan id. Aliquam a feugiat diam, ac vestibulum lacus. Nulla viverra nunc elementum varius
+		interdum.
+	</p>
+</section>
