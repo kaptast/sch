4165aa4c92a7a52f6022353f929641edaea01617
kaptast <kaptas.tamas@gmail.com>
2025-02-25 22:25:12 +0100

    style the language switchers

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
+	import { type AvailableLanguageTag, languageTag } from '$lib/paraglide/runtime';
 	import { i18n } from '$lib/i18n';
 	import { page } from '$app/state';
 	import { goto } from '$app/navigation';
@@ -25,13 +25,36 @@
 
 <header
 	style="--header-opacity: {opacity}%; --heading-padding: {padding}px"
-class="fixed top-0 right-0 left-0 z-50 flex w-full items-center justify-between bg-yellow-700/[var(--header-opacity)] py-[var(--heading-padding)] pr-5 pl-10 transition-opacity"
+	class="fixed top-0 right-0 left-0 z-50 flex w-full items-center justify-between bg-yellow-700/[var(--header-opacity)] py-[var(--heading-padding)] pr-5 pl-10 transition-opacity"
 >
 	<h1 class="text-4xl font-bold text-white">SCH</h1>
 	<MenuOpener class="lg:hidden" />
-	<div class="hidden lg:block">
-		<button onclick={() => switchToLanguage('en')}>en</button>
-		<button onclick={() => switchToLanguage('de')}>de</button>
-		<button onclick={() => switchToLanguage('hu')}>hu</button>
+	<div class="hidden gap-1 text-white lg:flex lg:items-center">
+		<button
+			class="pointer block underline transition-all hover:decoration-white {isActualLanguageTag(
+				'en'
+			)
+				? 'decoration-white'
+				: 'decoration-transparent'}"
+			onclick={() => switchToLanguage('en')}>en</button
+		>
+		<div class="h-4 w-px bg-white"></div>
+		<button
+			class="pointer block underline transition-all hover:decoration-white {isActualLanguageTag(
+				'de'
+			)
+				? 'decoration-white'
+				: 'decoration-transparent'}"
+			onclick={() => switchToLanguage('de')}>de</button
+		>
+		<div class=" h-4 w-px bg-white"></div>
+		<button
+			class="pointer block underline transition-all hover:decoration-white {isActualLanguageTag(
+				'hu'
+			)
+				? 'decoration-white'
+				: 'decoration-transparent'}"
+			onclick={() => switchToLanguage('hu')}>hu</button
+		>
 	</div>
 </header>
