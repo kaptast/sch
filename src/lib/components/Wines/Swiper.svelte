2b7f4fdf6c8d1006825da5a129362fe5b8291fb5
kaptast <kaptas.tamas@gmail.com>
2025-03-01 10:24:36 +0100

    create a wine swiper

--- src/lib/components/Wines/Swiper.svelte
+++ src/lib/components/Wines/Swiper.svelte
@@ -1,1 +1,20 @@
-<section></section>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Wine from './Wine.svelte';
+	import { register } from 'swiper/element/bundle';
+	register();
+
+	let swiperEl: HTMLElement;
+
+	onMount(() => {
+		swiperEl.initialize();
+	});
+</script>
+
+<section class="bg-amber-50">
+	<swiper-container bind:this={swiperEl} slides-per-view="1" init="false" loop="true">
+		<Wine name="Pölöskei Muskotály" year="2023" image="wine1.png" />
+		<Wine name="Maribor" year="2022" image="wine2.png" />
+		<Wine name="Kadarka" year="2022" image="wine3.png" />
+	</swiper-container>
+</section>
---
+++ src/lib/components/Wines/Wine.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let { image = '', name = '', year = '' } = $props();
+</script>
+
+<swiper-slide class="grid grid-cols-2">
+	<div class="row-span-2">
+		<img src={image} alt={name} class="h-52 object-contain" />
+	</div>
+
+	<div class="uppercase">
+		{name}
+	</div>
+
+	<div>
+		{year}
+	</div>
+</swiper-slide>
