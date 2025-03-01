763f2dfed08070100d1bbe8cf45787997db5d59f
kaptast <kaptas.tamas@gmail.com>
2025-03-01 11:08:44 +0100

    initial styling of the wine swiper

--- src/lib/components/Wines/Swiper.svelte
+++ src/lib/components/Wines/Swiper.svelte
@@ -6,15 +6,57 @@
 
 	let swiperEl: HTMLElement;
 
+	const params = {
+		injectStyles: [
+			`
+      .swiper-pagination-bullet {
+				width: 3rem;
+        font-size: 3rem;
+				line-height: 3.5rem;
+        color: white;
+				background: none;
+				opacity: 1;
+      }
+
+      .swiper-pagination-bullet-active {
+        color: #ff9900;
+				text-decoration: underline;
+      }
+      `
+		],
+		pagination: {
+			clickable: true,
+			renderBullet: function (index, className) {
+				return '<span class="' + className + '">' + (index + 1) + '</span>';
+			}
+		}
+	};
+
 	onMount(() => {
+		Object.assign(swiperEl, params);
 		swiperEl.initialize();
 	});
 </script>
 
-<section class="bg-amber-50">
-	<swiper-container bind:this={swiperEl} slides-per-view="1" init="false" loop="true">
+<section class="relative bg-amber-200 py-14">
+	<swiper-container
+		bind:this={swiperEl}
+		slides-per-view="1"
+		init="false"
+		loop="true"
+		pagination="true"
+	>
 		<Wine name="Pölöskei Muskotály" year="2023" image="wine1.png" />
 		<Wine name="Maribor" year="2022" image="wine2.png" />
 		<Wine name="Kadarka" year="2022" image="wine3.png" />
 	</swiper-container>
 </section>
+
+<style>
+	swiper-container::part(pagination) {
+		top: 50%;
+		left: 0;
+		transform: translateY(-50%);
+		width: 50%;
+	}
+</style>
--- src/lib/components/Wines/Wine.svelte
+++ src/lib/components/Wines/Wine.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
+	import Heading from '../Heading.svelte';
+
 	let { image = '', name = '', year = '' } = $props();
 </script>
 
-<swiper-slide class="grid grid-cols-2">
-	<div class="row-span-2">
-		<img src={image} alt={name} class="h-52 object-contain" />
+<swiper-slide class="grid grid-cols-2 items-center">
+	<div class="row-span-2 justify-self-end">
+		<img src={image} alt={name} class="h-96 object-contain" />
 	</div>
 
-	<div class="uppercase">
-		{name}
+	<div class="justify-self-center">
+		<Heading text={name} />
 	</div>
 
-	<div>
+	<div class="justify-self-center">
 		{year}
 	</div>
 </swiper-slide>
